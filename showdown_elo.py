79477409deadfc831f84624e24e1f84b410866c7
Rocco <roccodev.business@gmail.com>
2020-04-13 15:18:27 +0200

    Switch to urllib2 for support out of the box

--- showdown_elo.py
+++ showdown_elo.py
@@ -4,7 +4,8 @@
 # https://opensource.org/licenses/MIT
 
 import obspython as obs
-import urllib3.request
+import urllib.request
+import urllib.error
 import json
 
 # Configuration
@@ -14,21 +15,11 @@ interval = 5
 source_name = ""
 str_format = ""
 
-source_active = False
-
-http = urllib3.PoolManager()
-
 # Script meta
 
 
 def script_description():
     return "Display your Pokemon Showdown ELO in a text source\n\nInfo and help: https://github.com/RoccoDev/showdown-elo-obs"
-
-
-def script_load(settings):
-    sh = obs.obs_get_signal_handler()
-    obs.signal_handler_connect(sh, "source_activate", src_active)
-    obs.signal_handler_connect(sh, "source_deactivate", src_inactive)
 
 
 def script_update(settings):
@@ -71,7 +62,7 @@ def script_properties():
     if sources is not None:
         for source in sources:
             source_id = obs.obs_source_get_id(source)
-            if source_id == "text_gdiplus" or source_id == "text_ft2_source":
+            if source_id == "text_gdiplus" or source_id == "text_gdiplus_v2" or source_id == "text_ft2_source":
                 name = obs.obs_source_get_name(source)
                 obs.obs_property_list_add_string(p, name, name)
 
@@ -84,24 +75,6 @@ def script_properties():
     return props
 
 
-def activate(cd, activating):
-    global source_active
-
-    src = obs.calldata_source(cd, "source")
-    if src is not None:
-        name = obs.obs_source_get_name(src)
-        if name == source_name:
-            source_active = activating
-
-
-def src_active(cd):
-    activate(cd, True)
-
-
-def src_inactive(cd):
-    activate(cd, False)
-
-
 def update_text():
     global username
     global format
@@ -110,22 +83,20 @@ def update_text():
     global str_format
 
     source = obs.obs_get_source_by_name(source_name)
-    if source is not None and source_active:
+    if source is not None and obs.obs_source_active(source):
         try:
-            r = http.request("GET",
-                             "http://play.pokemonshowdown.com/%7e%7eshowdown/action.php?act=ladderget&user=" + username,
-                             headers={
-                                 'User-Agent': 'ShowdownELOOBS/1.0'
-                             })
-            data = json.loads(r.data.decode('utf-8')[1:])
+            opener = urllib.request.build_opener()
+            opener.addheaders = [('User-Agent', 'ShowdownELOOBS/1.0')]
+            r = opener.open("http://play.pokemonshowdown.com/%7e%7eshowdown/action.php?act=ladderget&user=" + username)
+            data = json.loads(r.read().decode('utf-8')[1:])
             obj = [o for o in data if o['formatid'] == format]
 
             # Statistics
             elo = 1000 if len(obj) == 0 else obj[0]['elo']
             gxe = "?" if len(obj) == 0 else str(obj[0]['gxe']) + '%'
-            wins = 0 if len(obj) == 0 else obj[0]['w']
-            losses = 0 if len(obj) == 0 else obj[0]['l']
-            ties = 0 if len(obj) == 0 else obj[0]['t']
+            wins = 0 if len(obj) == 0 else int(obj[0]['w'])
+            losses = 0 if len(obj) == 0 else int(obj[0]['l'])
+            ties = 0 if len(obj) == 0 else int(obj[0]['t'])
             games = wins + losses + ties
             user = username if len(obj) == 0 else obj[0]['username']
 
@@ -138,9 +109,9 @@ def update_text():
             obs.obs_source_update(source, settings)
             obs.obs_data_release(settings)
 
-        except urllib3.exceptions.HTTPError as err:
+        except urllib.error.URLError as err:
             obs.script_log(obs.LOG_WARNING,
-                           "Error opening URL: " + err)
+                           "Error opening URL: " + err.reason)
             obs.remove_current_callback()
 
         obs.obs_source_release(source)
